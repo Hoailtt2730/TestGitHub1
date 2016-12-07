013c406c8e1570ad4a962c9dc7b87126cdfc65bc
hoailtt <hoailtt2730@gmail.com>
2016-12-07 10:23:10 +0700

    set text gravity

--- app/src/main/java/jp/co/systena/android/car/testgithub/MainActivity.java
+++ app/src/main/java/jp/co/systena/android/car/testgithub/MainActivity.java
@@ -2,6 +2,7 @@ package jp.co.systena.android.car.testgithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView textView = (TextView) findViewById(R.id.test_tv);
         textView.setText("Nhoi");
+        textView.setGravity(Gravity.CENTER);
     }
 }
